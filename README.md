a9053723848779539e48a2f2c4c79f86a92922c6
Oleh Sobchuk <oleg.sobchuk@gmail.com>
2024-01-24 14:01:04 +0200

    Get acquainted with Fiber framework

---
+++ README.md
@@ -0,0 +1,3 @@
+## OBSERVATORY
+
+This is lightweight application for getting availability statuses for devices in the network. To be more clear this application for getting information about connection among devices.
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/logger"
+)
+
+func main() {
+	app := fiber.New()
+	app.Use(logger.New(logger.Config{
+		Format: "${time} | ${status} | ${latency} | ${ip} | ${method} | ${path} ${queryParams} | ${error} \n",
+	}))
+
+	app.Get("/", func(c fiber.Ctx) error {
+		return c.SendString("Hello world!")
+	})
+
+	app.Listen(":3000")
+}
